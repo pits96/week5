1d9d334e7dbe38fdb144cfe3f4f217c35f17df6c
Aaron Tadina <atadina@mail.greenriver.edu>
2020-05-04 17:34:16 -0400

    added content for info.html

--- index.php
+++ index.php
@@ -8,7 +8,18 @@ require_once ("vendor/autoload.php");
 $f3 = Base::instance();
 
 //Default route
-$f3->route('GET /',function(){
+$f3->route('GET /',function($f3){
+    //Create variables in the F3 hive
+    $f3->set('username','jshmo');
+    $f3->set('password',sha1('Password01'));
+    $f3->set('title','Working with Templates');
+    $f3->set('color','Purple');
+    $f3->set('radius',10);
+    $f3->set('fruits',array('apple','orange','banana'));
+    $f3->set('bookmarks',array('http://www.cnet.com',
+                                'http://www.reddit.com/r/news',
+                                'http://www.edition.cnn.com/sport'));
+    $f3->set('desserts', array('chocolate'=> 'Chocolate Mousse','vanilla'=>'Vanilla Custard','strawberry'=>'Strawberry Shortcake'));
     $view = new Template();
     echo $view->render("views/info.html");
 });
